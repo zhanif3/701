03ca6a3ae3f51e26a9a239b9338a881e2ac45582
Zachary Hanif <zachary.hanif@capitalone.com>
2019-05-06 06:56:11 -0400

    oops

--- src/run_multi_experiments.sh
+++ src/run_multi_experiments.sh
@@ -2,41 +2,41 @@
 
 touch multi_threaded_metrics
 
-target/release/all_in_one bwt/data/100_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/200_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/300_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/400_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/500_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/600_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/700_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/800_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/900_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/100_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/200_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/300_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/400_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/500_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/600_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/700_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/800_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/900_000_dna ggtttccc >> multi_threaded_metrics
 
-target/release/all_in_one bwt/data/1_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/2_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/3_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/4_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/5_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/6_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/7_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/8_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/9_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/1_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/2_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/3_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/4_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/5_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/6_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/7_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/8_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/9_000_000_dna ggtttccc >> multi_threaded_metrics
 
 
-target/release/all_in_one bwt/data/10_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/11_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/12_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/13_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/14_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/15_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/16_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/17_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/18_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/19_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/10_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/11_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/12_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/13_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/14_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/15_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/16_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/17_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/18_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/19_000_000_dna ggtttccc >> multi_threaded_metrics
 
-target/release/all_in_one bwt/data/20_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/21_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/22_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/23_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/24_000_000_dna ggtttccc >> multi_threaded_metrics
-target/release/all_in_one bwt/data/25_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/20_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/21_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/22_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/23_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/24_000_000_dna ggtttccc >> multi_threaded_metrics
+target/release/all_in_one data/25_000_000_dna ggtttccc >> multi_threaded_metrics
