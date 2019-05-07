b5f5624ab66734f5a4af8ae9fdd4f87acd90f9cd
Zachary Hanif <zachary.hanif@capitalone.com>
2019-05-07 16:26:06 -0400

    fixing query again

---
+++ run_single_experiments_other.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+touch other_single_metrics
+
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/1000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/2000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/3000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/4000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/5000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/6000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/7000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/8000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/9000_dna TTAGAGAAA >> other_single_metrics
+
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/10000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/20000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/30000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/40000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/50000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/60000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/70000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/80000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/90000_dna TTAGAGAAA >> other_single_metrics
+
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/100000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/110000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/120000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/130000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/140000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/150000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/160000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/170000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/180000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/190000_dna TTAGAGAAA >> other_single_metrics
+
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/200000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/210000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/220000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/230000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/240000_dna TTAGAGAAA >> other_single_metrics
+RAYON_NUM_THREADS=1 target/release/all_in_one other_data/250000_dna TTAGAGAAA >> other_single_metrics
