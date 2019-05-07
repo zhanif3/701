9f037ac6a517ab9c472a1c5f1debe171bb994774
Zachary Hanif <zachary.hanif@capitalone.com>
2019-05-07 15:52:20 -0400

    Adding new datasets

---
+++ other_data/generate_data.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+head -n 1000 27687_ref_fErpCal1.1_chr17.fa > 1000_dna
+head -n 2000 27687_ref_fErpCal1.1_chr17.fa > 2000_dna
+head -n 3000 27687_ref_fErpCal1.1_chr17.fa > 3000_dna
+head -n 4000 27687_ref_fErpCal1.1_chr17.fa > 4000_dna
+head -n 5000 27687_ref_fErpCal1.1_chr17.fa > 5000_dna
+head -n 6000 27687_ref_fErpCal1.1_chr17.fa > 6000_dna
+head -n 7000 27687_ref_fErpCal1.1_chr17.fa > 7000_dna
+head -n 8000 27687_ref_fErpCal1.1_chr17.fa > 8000_dna
+head -n 9000 27687_ref_fErpCal1.1_chr17.fa > 9000_dna
+
+head -n 10000 27687_ref_fErpCal1.1_chr17.fa > 10000_dna
+head -n 20000 27687_ref_fErpCal1.1_chr17.fa > 20000_dna
+head -n 30000 27687_ref_fErpCal1.1_chr17.fa > 30000_dna
+head -n 40000 27687_ref_fErpCal1.1_chr17.fa > 40000_dna
+head -n 50000 27687_ref_fErpCal1.1_chr17.fa > 50000_dna
+head -n 60000 27687_ref_fErpCal1.1_chr17.fa > 60000_dna
+head -n 70000 27687_ref_fErpCal1.1_chr17.fa > 70000_dna
+head -n 80000 27687_ref_fErpCal1.1_chr17.fa > 80000_dna
+head -n 90000 27687_ref_fErpCal1.1_chr17.fa > 90000_dna
+
+head -n 100000 27687_ref_fErpCal1.1_chr17.fa > 100000_dna
+head -n 110000 27687_ref_fErpCal1.1_chr17.fa > 110000_dna
+head -n 120000 27687_ref_fErpCal1.1_chr17.fa > 120000_dna
+head -n 130000 27687_ref_fErpCal1.1_chr17.fa > 130000_dna
+head -n 140000 27687_ref_fErpCal1.1_chr17.fa > 140000_dna
+head -n 150000 27687_ref_fErpCal1.1_chr17.fa > 150000_dna
+head -n 160000 27687_ref_fErpCal1.1_chr17.fa > 160000_dna
+head -n 170000 27687_ref_fErpCal1.1_chr17.fa > 170000_dna
+head -n 180000 27687_ref_fErpCal1.1_chr17.fa > 180000_dna
+head -n 190000 27687_ref_fErpCal1.1_chr17.fa > 190000_dna
+
+head -n 200000 27687_ref_fErpCal1.1_chr17.fa > 200000_dna
+head -n 210000 27687_ref_fErpCal1.1_chr17.fa > 210000_dna
+head -n 220000 27687_ref_fErpCal1.1_chr17.fa > 220000_dna
+head -n 230000 27687_ref_fErpCal1.1_chr17.fa > 230000_dna
+head -n 240000 27687_ref_fErpCal1.1_chr17.fa > 240000_dna
+head -n 250000 27687_ref_fErpCal1.1_chr17.fa > 250000_dna
